c47dc155f6253678530529a10b75fbcc524c6769
antonio.sanchez <antonio.sanchez@leanmind.es>
2024-08-09 21:47:08 +0100

    improve algorithms performance

--- src/DataStructures/Stack/PalindromicSequence/main.cpp
+++ src/DataStructures/Stack/PalindromicSequence/main.cpp
@@ -6,9 +6,11 @@
 
 int main()
 {
-    std::string input;
-
-    std::cout << "Enter a sequence of numbers separated by spaces:" << std::endl;
+    std::cout << "Enter a sequence of numbers separated by spaces. \n";
+    std::cout << "First number should be the number of elements and the rest the sequence of numbers. \n";
+    std::cout << "Example: 3 1 2 1 ('3' is the number of elements and '1 2 1' the sequence of numbers to check). \n";
+    std::cout << "Your sequence: ";
+    std::string input = "";
     std::getline(std::cin, input);
 
     std::stringstream inputStream(input);
@@ -16,32 +18,65 @@ int main()
     std::string sequenceElement;
     while(std::getline(inputStream, sequenceElement, ' '))
     {
-        numberSequence.push_back(std::stoi(sequenceElement));
+        try
+        {
+            numberSequence.push_back(std::stoi(sequenceElement));
+        } 
+        catch (const std::invalid_argument&)
+        {
+            std::cout << "[ERROR] Input should be a sequence of integers." << std::endl;
+            return 1;
+        }
     }
 
-    std::stack<int> reversedNumberSequece;
-    for(const int& number : numberSequence)
+    if(numberSequence.empty())
     {
-        reversedNumberSequece.push(number);
-    }
-
-    if(numberSequence.size() != reversedNumberSequece.size())
-    {
-        std::cout << "No" << std::endl;
+        std::cout << "[INFO] Sequence with no elements can not be checked as palindromic or not." << std::endl;
         return 1;
     }
 
-    for(int i = 0; i < numberSequence.size(); ++i)
+    int numberOfElements = numberSequence[0];
+    if(numberOfElements == 0)
     {
-        auto lastNumber = reversedNumberSequece.top();
-        reversedNumberSequece.pop();
-        if(numberSequence[i] != lastNumber)
+        std::cout << "[INFO] Sequence with no elements can not be checked as palindromic or not." << std::endl;
+        return 1;
+    } 
+    if(numberOfElements != (numberSequence.size() - 1))
+    {
+        std::cout << "[ERROR] The number of elements in the sequence not match with the number of the elemenets defined in the input." << std::endl;
+        return 1;
+
+    }
+    if(numberOfElements == 1)
+    {
+        std::cout << "Yes" << std::endl;
+        return 0;
+    }
+
+    int intermediateElementOfTheSequence = (numberOfElements / 2) + 1;
+    
+    std::stack<int> elementsStack;
+    for(int i = 1; i < intermediateElementOfTheSequence; ++i) 
+    {
+        elementsStack.push(numberSequence[i]);
+    }
+
+    if(numberOfElements % 2 != 0)
+    {
+        intermediateElementOfTheSequence += 1;
+    }
+
+    for(int i = intermediateElementOfTheSequence; i <= numberOfElements ; ++i) 
+    {
+        if(numberSequence[i] != elementsStack.top())
         {
-            std::cout << "No" << std::endl;
+            std::cout << "NO" << std::endl;
             return 1;
         }
+        elementsStack.pop();
     }
 
     std::cout << "Yes" << std::endl;
     return 0;
 }
+
