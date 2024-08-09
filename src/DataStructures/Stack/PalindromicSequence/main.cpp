8d92227c00f2b97f2874de62fb38b5fb09404d94
antonio.sanchez <antonio.sanchez@leanmind.es>
2024-08-09 17:28:26 +0100

    first solution for palindromic sequence

--- src/DataStructures/Stack/PalindromicSequence/main.cpp
+++ src/DataStructures/Stack/PalindromicSequence/main.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <stack>
 
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
+    std::string input;
+
+    std::cout << "Enter a sequence of numbers separated by spaces:" << std::endl;
+    std::getline(std::cin, input);
+
+    std::stringstream inputStream(input);
+    std::vector<int> numberSequence;
+    std::string sequenceElement;
+    while(std::getline(inputStream, sequenceElement, ' '))
+    {
+        numberSequence.push_back(std::stoi(sequenceElement));
+    }
+
+    std::stack<int> reversedNumberSequece;
+    for(const int& number : numberSequence)
+    {
+        reversedNumberSequece.push(number);
+    }
+
+    if(numberSequence.size() != reversedNumberSequece.size())
+    {
+        std::cout << "No" << std::endl;
+        return 1;
+    }
+
+    for(int i = 0; i < numberSequence.size(); ++i)
+    {
+        auto lastNumber = reversedNumberSequece.top();
+        reversedNumberSequece.pop();
+        if(numberSequence[i] != lastNumber)
+        {
+            std::cout << "No" << std::endl;
+            return 1;
+        }
+    }
+
+    std::cout << "Yes" << std::endl;
     return 0;
 }
