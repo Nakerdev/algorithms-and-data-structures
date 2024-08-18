6757f3cd6a8b2edf5a44e83e81ea937e19729415
antonio.sanchez <antonio.sanchez@leanmind.es>
2024-08-18 22:25:35 +0100

    extract method

--- src/DataStructures/Stack/SmallerOnTheLeft/main.cpp
+++ src/DataStructures/Stack/SmallerOnTheLeft/main.cpp
@@ -2,6 +2,23 @@
 #include <vector>
 #include <stack>
 
+int FindPreviousSmallerElement(const std::stack<int>& alreadyComputedNumbers, const int& number)
+{
+    int previousSmallerNumber = -1;
+    std::stack<int> alreadyComputedNumbers_copy = alreadyComputedNumbers;
+    while (!alreadyComputedNumbers_copy.empty()) 
+    {
+        auto lastNumber = alreadyComputedNumbers_copy.top();
+        if(lastNumber < number)
+        {
+            previousSmallerNumber = lastNumber;
+            break;
+        }
+        alreadyComputedNumbers_copy.pop();
+    }
+    return previousSmallerNumber;
+}
+
 int main()
 {
     std::vector<int> numberSequence = { 2, 5, 3, 7, 8, 3, 1, 9 }; //output: -1, 2, 2, 3, 7, 2, -1, 1
@@ -17,21 +34,8 @@ int main()
             numberSequence[i] = -1;
             continue;
         }
-
-        int previousSmallerNumber = -1;
-        std::stack<int> alreadyComputedNumbers_copy = alreadyComputedNumbers;
-        while (!alreadyComputedNumbers_copy.empty()) 
-        {
-            auto lastNumber = alreadyComputedNumbers_copy.top();
-            if(lastNumber < number)
-            {
-                previousSmallerNumber = lastNumber;
-                break;
-            }
-            alreadyComputedNumbers_copy.pop();
-        }
-        numberSequence[i] = previousSmallerNumber;
-
+        
+        numberSequence[i] = FindPreviousSmallerElement(alreadyComputedNumbers, number);
         alreadyComputedNumbers.push(number);
     }
 
