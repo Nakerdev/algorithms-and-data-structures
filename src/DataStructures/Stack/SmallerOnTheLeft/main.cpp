48b416564fd0238cf1561f9328bd3af78c9adbd7
antonio.sanchez <antonio.sanchez@leanmind.es>
2024-08-18 22:22:58 +0100

    simplify code

--- src/DataStructures/Stack/SmallerOnTheLeft/main.cpp
+++ src/DataStructures/Stack/SmallerOnTheLeft/main.cpp
@@ -4,8 +4,8 @@
 
 int main()
 {
-    //std::vector<int> numberSequence = { 2, 5, 3, 7, 8, 1, 9 }; //output: -1, 2, 2, 3, 7, -1, 1
-    std::vector<int> numberSequence = { 5, 7, 4, 9, 8, 10 }; //output: -1, 5, -1, 4, 4, 8
+    std::vector<int> numberSequence = { 2, 5, 3, 7, 8, 3, 1, 9 }; //output: -1, 2, 2, 3, 7, 2, -1, 1
+    //std::vector<int> numberSequence = { 5, 7, 4, 9, 8, 10 }; //output: -1, 5, -1, 4, 4, 8
     std::stack<int> alreadyComputedNumbers;
 
     for(int i = 0; i < numberSequence.size(); ++i)
@@ -18,27 +18,19 @@ int main()
             continue;
         }
 
-        auto lastSmallerNumber = alreadyComputedNumbers.top();
-        if(lastSmallerNumber < number)
-        { 
-            numberSequence[i] = lastSmallerNumber;
-        }
-        else 
+        int previousSmallerNumber = -1;
+        std::stack<int> alreadyComputedNumbers_copy = alreadyComputedNumbers;
+        while (!alreadyComputedNumbers_copy.empty()) 
         {
-             int previousSmallerNumber = -1;
-             std::stack<int> alreadyComputedNumbers_copy = alreadyComputedNumbers;
-             while (!alreadyComputedNumbers_copy.empty()) 
-             {
-                auto lastNumber = alreadyComputedNumbers_copy.top();
-                if(lastNumber < number)
-                {
-                    previousSmallerNumber = lastNumber;
-                    break;
-                }
-                alreadyComputedNumbers_copy.pop();
+            auto lastNumber = alreadyComputedNumbers_copy.top();
+            if(lastNumber < number)
+            {
+                previousSmallerNumber = lastNumber;
+                break;
             }
-            numberSequence[i] = previousSmallerNumber;
+            alreadyComputedNumbers_copy.pop();
         }
+        numberSequence[i] = previousSmallerNumber;
 
         alreadyComputedNumbers.push(number);
     }
