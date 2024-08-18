012d0966c2395d9e65113bbd75e060119d1319cf
antonio.sanchez <antonio.sanchez@leanmind.es>
2024-08-18 22:26:10 +0100

    remove comment

--- src/DataStructures/Stack/SmallerOnTheLeft/main.cpp
+++ src/DataStructures/Stack/SmallerOnTheLeft/main.cpp
@@ -21,8 +21,7 @@ int FindPreviousSmallerElement(const std::stack<int>& alreadyComputedNumbers, co
 
 int main()
 {
-    std::vector<int> numberSequence = { 2, 5, 3, 7, 8, 3, 1, 9 }; //output: -1, 2, 2, 3, 7, 2, -1, 1
-    //std::vector<int> numberSequence = { 5, 7, 4, 9, 8, 10 }; //output: -1, 5, -1, 4, 4, 8
+    std::vector<int> numberSequence = { 2, 5, 3, 7, 8, 1, 9 };
     std::stack<int> alreadyComputedNumbers;
 
     for(int i = 0; i < numberSequence.size(); ++i)
